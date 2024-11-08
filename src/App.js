a35f9099f609d841b84f1b14dd136a10cc62824e
IvanR <ivan.ramirezf@gmail.com>
2024-11-09 00:28:16 +1000

    Added skeleton components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Form from "./Form";
+import Logo from "./Logo";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+import "./index.css";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
 export default App;
