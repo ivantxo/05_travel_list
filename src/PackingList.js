208bb676e91fb031d661dfef4a074261c12ec355
IvanR <ivan.ramirezf@gmail.com>
2024-11-09 00:58:39 +1000

    Rendering the item's list

--- src/PackingList.js
+++ src/PackingList.js
@@ -1,5 +1,29 @@
+import Item from "./Item";
+
+const inialItems = [
+  {
+    id: 1,
+    description: "Passports",
+    quantity: 2,
+    packed: false,
+  },
+  {
+    id: 2,
+    description: "Socks",
+    quantity: 12,
+    packed: false,
+  },
+];
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {inialItems.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default PackingList;
