b84e9c67645be07629c958f74efc1e66e8dd500f
IvanR <ivan.ramirezf@gmail.com>
2024-11-16 00:13:18 +1000

    Adding state to Form component

--- src/Form.js
+++ src/Form.js
@@ -3,6 +3,11 @@ import { useState } from "react";
 function Form() {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -11,6 +16,8 @@ function Form() {
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
+
+    handleAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
